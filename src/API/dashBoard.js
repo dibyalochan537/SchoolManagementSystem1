0949ca90aa3dda8d4fd1d1207d65d3242f706b0f
dibyalochan537 <dibyalochandash537@gmail.com>
2025-06-21 21:56:17 +0530

    Update dashBoard.js

--- src/API/dashBoard.js
+++ src/API/dashBoard.js
@@ -2,7 +2,7 @@
 const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 async function adminDashboardFetchData(endpoint) {
     try {
-        const response = await fetch(`${API_BASE_URL}/${endpoint}`);
+        const response = await fetch(`${API_BASE_URL}${endpoint}`);
         if (!response.ok) {
             throw new Error(`Failed to fetch data from ${endpoint}`);
         }
