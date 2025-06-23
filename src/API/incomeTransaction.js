d4fa1b4b725562e05602e79c230c9513006119bd
dibyalochan537 <dibyalochandash537@gmail.com>
2025-06-23 16:25:46 +0530

    Update incomeTransaction.js

--- src/API/incomeTransaction.js
+++ src/API/incomeTransaction.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://schoolmanagement-api-67878057783.us-central1.run.app/api";
+const API_BASE_URL =  `${import.meta.env.VITE_API_URL}/api`;
 async function transactionDetails(endpoint) {
     try {
         const response = await fetch(`${API_BASE_URL}/${endpoint}`);
